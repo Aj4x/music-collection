45931396045190edfc089c1fb4a48a8a5fccd255
david <d4v1d666@gmail.com>
2025-03-01 01:24:49 +0000

    added basic server with sample templ component

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"context"
+	"errors"
+	"github.com/Aj4x/music-collection/views"
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"log/slog"
+	"net/http"
+)
 
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/", func(c echo.Context) error {
+		return render(c, http.StatusOK, views.Hello("World"))
+	})
+	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("failed to start server", "error", err)
+	}
+}
+
+func render(c echo.Context, status int, t templ.Component) error {
+	c.Response().Writer.WriteHeader(status)
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTMLCharsetUTF8)
+	err := t.Render(context.Background(), c.Response().Writer)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "failed to render component")
+	}
+	return nil
 }
